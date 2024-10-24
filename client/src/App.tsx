b7a3f90631ac4c6d64e693ca360a218950586806
OrangeMintz <Nickzgacus@gmail.com>
2024-10-24 21:36:11 +0800

    learning basics in typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,40 @@
 import './App.css'
-import { Person } from './component/person'
 
 function App() {
 
+  
+  type Person = {
+    name: string,
+    age: number,
+    hobby?: string,
+    sex: string,
+    student: boolean
+  };
 
+  let person: Person = {
+    name: "Djeikuje Nickolai C. Gacus",
+    age: 22,
+    hobby: "Gaming, Coding",
+    sex: "Male",
+    student: true
+  }
+
+  person = {
+    name: "Jhon Kert J. Talha",
+    age: 22,
+    sex: "Male",
+    student: true
+  }
 
   return (
     <>
-      <Person name={"Djeikuje Nickolai C. Gacus"} age={22} isStudent={true} />
-      <Person name={"Jhon Kert J. Talha"} age={23} isStudent={false} />
+      <div>
+        <h1>{person.name}</h1>
+        <h1>{person.age}</h1>
+        <h1>{person.hobby}</h1>
+        <h1>{person.sex}</h1>
+        <h1>{person.student ? "True" : "False"}</h1>
+      </div>
     </>
   );
 }
