e74eda3f6b800ca51a5ea86689d092930e4a6cf6
OrangeMintz <Nickzgacus@gmail.com>
2024-10-28 21:49:44 +0800

    - uses ESlint9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
 
-  
-  type Person = {
-    name: string,
-    age: number,
-    hobby?: string,
-    sex: string,
-    student: boolean
-  };
-
-  let person: Person = {
-    name: "Djeikuje Nickolai C. Gacus",
-    age: 22,
-    hobby: "Gaming, Coding",
-    sex: "Male",
-    student: true
-  }
-
-  person = {
-    name: "Jhon Kert J. Talha",
-    age: 22,
-    sex: "Male",
-    student: true
-  }
-
   return (
     <>
-      <div>
-        <h1>{person.name}</h1>
-        <h1>{person.age}</h1>
-        <h1>{person.hobby}</h1>
-        <h1>{person.sex}</h1>
-        <h1>{person.student ? "True" : "False"}</h1>
-      </div>
     </>
   );
 }
 
-export default App
+export default App;
