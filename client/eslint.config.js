84e9e662cf05cb1c5e1176da9233d208cf433c8f
OrangeMintz <Nickzgacus@gmail.com>
2024-11-04 02:14:46 +0800

    - fck eslint 9

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,7 +5,7 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
-      semi: "warn",
+      semi: "none",
     },
   },
   {
