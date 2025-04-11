21fa72063c8aa41a265db92988a78236e3bac281
OrangeMintz <nickzgacus@gmail.com>
2025-04-11 15:22:26 +0800

    Bugfix: fix toaster

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, Slide } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
 // import App from "./App.tsx";
+
+import "./toast.css";
 
 // import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -12,7 +15,19 @@ import router from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Slide}
+    />
     <RouterProvider router={router} />
   </StrictMode>
 );
