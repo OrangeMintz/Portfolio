cee2ad2872f52ead4264feff65ec43fd844b9f45
OrangeMintz <nickzgacus@gmail.com>
2025-04-14 18:33:45 +0800

    Bugfix: sidebar header toggle

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -6,6 +6,11 @@ function Sidebar() {
   const location = useLocation();
   const [activeHash, setActiveHash] = useState("#home");
 
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarOpen((prev) => !prev);
+  };
+
   // Update active section when scrolling (Home page)
   useEffect(() => {
     const sections = document.querySelectorAll("section");
@@ -40,92 +45,106 @@ function Sidebar() {
   };
 
   return (
-    <header id="header" className="header dark-background d-flex flex-column">
-      <i className="header-toggle d-xl-none bi bi-list"></i>
+    <>
+      <i
+        className="header-toggle d-xl-none bi bi-list"
+        onClick={toggleSidebar}
+      />
 
-      <div className="profile-img">
-        <img
-          src="portfolio/assets/img/profile.png"
-          alt=""
-          className="img-fluid rounded-circle"
-        />
-      </div>
-
-      <Link
-        to="/"
-        className="logo d-flex align-items-center justify-content-center"
+      <header
+        id="header"
+        className={`header dark-background d-flex flex-column ${
+          isSidebarOpen ? "show-sidebar" : "hide-sidebar"
+        }`}
       >
-        <h1 className="sitename">Djeikuje Nickolai</h1>
-      </Link>
+        {/* <i className="header-toggle d-xl-none bi bi-list"></i> */}
+        <div className="profile-img">
+          <img
+            src="portfolio/assets/img/profile.png"
+            alt=""
+            className="img-fluid rounded-circle"
+          />
+        </div>
 
-      <div className="social-links text-center">
-        <Link to="https://x.com/MintRange" className="twitter">
-          <i className="bi bi-twitter-x"></i>
-        </Link>
-        <Link to="https://facebook.com/OrangeMintz" className="facebook">
-          <i className="bi bi-facebook"></i>
-        </Link>
         <Link
-          to="https://www.instagram.com/OrangeMint57/"
-          className="instagram"
+          to="/"
+          className="logo d-flex align-items-center justify-content-center"
         >
-          <i className="bi bi-instagram"></i>
+          <h1 className="sitename">Djeikuje Nickolai</h1>
         </Link>
-        <Link
-          to="https://discordapp.com/users/505809822239948806"
-          className="discord"
-        >
-          <i className="bi bi-discord"></i>
-        </Link>
-        <Link to="https://github.com/OrangeMintz" className="github">
-          <i className="bi bi-github"></i>
-        </Link>
-      </div>
 
-      <nav id="navmenu" className="navmenu">
-        <ul>
-          <li>
-            <a href="/#home" className={isActive("#home") ? "active" : ""}>
-              <i className="bi bi-house navicon"></i> Home
-            </a>
-          </li>
-          <li>
-            <a href="/#about" className={isActive("#about") ? "active" : ""}>
-              <i className="bi bi-person navicon"></i> About
-            </a>
-          </li>
-          <li>
-            <a href="/#resume" className={isActive("#resume") ? "active" : ""}>
-              <i className="bi bi-file-earmark-text navicon"></i> Resume
-            </a>
-          </li>
-          <li>
-            <a
-              href="/#services"
-              className={isActive("#services") ? "active" : ""}
-            >
-              <i className="bi bi-hdd-stack navicon"></i> Services
-            </a>
-          </li>
-          <li>
-            <a
-              href="/#contact"
-              className={isActive("#contact") ? "active" : ""}
-            >
-              <i className="bi bi-envelope navicon"></i> Contacts
-            </a>
-          </li>
-          <li>
-            <a
-              href="/projects"
-              className={isActive("/projects") ? "active" : ""}
-            >
-              <i className="bi bi-stack navicon"></i> Projects
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <div className="social-links text-center">
+          <Link to="https://x.com/MintRange" className="twitter">
+            <i className="bi bi-twitter-x"></i>
+          </Link>
+          <Link to="https://facebook.com/OrangeMintz" className="facebook">
+            <i className="bi bi-facebook"></i>
+          </Link>
+          <Link
+            to="https://www.instagram.com/OrangeMint57/"
+            className="instagram"
+          >
+            <i className="bi bi-instagram"></i>
+          </Link>
+          <Link
+            to="https://discordapp.com/users/505809822239948806"
+            className="discord"
+          >
+            <i className="bi bi-discord"></i>
+          </Link>
+          <Link to="https://github.com/OrangeMintz" className="github">
+            <i className="bi bi-github"></i>
+          </Link>
+        </div>
+
+        <nav id="navmenu" className="navmenu">
+          <ul>
+            <li>
+              <a href="/#home" className={isActive("#home") ? "active" : ""}>
+                <i className="bi bi-house navicon"></i> Home
+              </a>
+            </li>
+            <li>
+              <a href="/#about" className={isActive("#about") ? "active" : ""}>
+                <i className="bi bi-person navicon"></i> About
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#resume"
+                className={isActive("#resume") ? "active" : ""}
+              >
+                <i className="bi bi-file-earmark-text navicon"></i> Resume
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#services"
+                className={isActive("#services") ? "active" : ""}
+              >
+                <i className="bi bi-hdd-stack navicon"></i> Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#contact"
+                className={isActive("#contact") ? "active" : ""}
+              >
+                <i className="bi bi-envelope navicon"></i> Contacts
+              </a>
+            </li>
+            <li>
+              <a
+                href="/projects"
+                className={isActive("/projects") ? "active" : ""}
+              >
+                <i className="bi bi-stack navicon"></i> Projects
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
 
